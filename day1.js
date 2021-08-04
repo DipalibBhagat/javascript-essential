9359ccfd28b8bc2e362c8fa4e358dec7ae2259c6
DipalibBhagat <87080008+DipalibBhagat@users.noreply.github.com>
2021-08-04 17:46:02 +0530

    day1 Assignment submitted.

---
+++ day1.js
@@ -0,0 +1,7 @@
+var a=10;
+var b=20;
+var add=a+b;
+var sub=b-a;
+var mult=a*b;
+var div=b/a;
+console.log(`Addition is: ${add} Subtraction is: ${sub} Multiplication is: ${mult} Division is: ${div}`);
